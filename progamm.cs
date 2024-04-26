19b6aaf04c0386142ac9309b45870e99cb5e66cb
opilane <opilane@HP-CZC0359NP3>
2024-04-26 12:30:09 +0300

    Create progamm.cs

---
+++ progamm.cs
@@ -0,0 +1,10 @@
+namespace Arvolol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        }
+    }
+}
